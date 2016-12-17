88a8a1ceab20570f28d93ec16ebdcf68a314c290
Nicolas Drufin <nicolas.drufin@ensc.fr>
2016-12-17 23:42:25 +0100

    first version of blog with style and about page

---
+++ content/pages/about.md
@@ -0,0 +1,12 @@
+Title: À propos
+
+Vous êtes sur le blog de Nicolas Drufin, ingénieur cogniticien spécialisé en interfaces homme-machine et big data.
+
+Vous trouverez ici :
+
+- des avis et comparatifs sur toutes sortes de visualisations de données
+- mes projets en cours et ceux auquels j'ai collaboré 
+
+Pour en savoir plus sur moi, n'hésitez pas à visiter [mon site portefolio](http://magikhaos.free.fr/nicolasdrufin).
+
+*[cogniticien]: De cognitique, traitement automatique de la connaissance
--- pelicanconf.py
+++ pelicanconf.py
@@ -24,14 +24,28 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+STATIC_PATHS = ['images', 'static']
+
+CUSTOM_CSS = 'static/custom_style.css'
+
+# Mardown extension
+# MD_EXTENSIONS = ['extra']
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('linkedin', 'http://fr.linkedin.com/pub/nicolas-drufin/'),
+          ('github', 'https://github.com/Starfight/'),
+          ('facebook', 'http://www.facebook.com/nicolas.drufin'),
+          ('twitter', 'https://twitter.com/Magikhaos'),
+          ('envelope-o', 'mailto:nicolas.drufin@ensc.fr'),
+          ('rss', '/feeds/all.atom.xml'))
 
 DEFAULT_PAGINATION = 10
 
